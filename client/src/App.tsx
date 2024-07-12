46d873e38272c110d8f82410dc9b3a8ff4f98481
Mete G <mete.guneysel@gmail.com>
2024-07-12 22:51:14 +0300

    Add dark mode for late night coding sessions

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,70 @@
 import './App.css'
-import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
-import { AppBar, Toolbar, CssBaseline, Container, Box, IconButton } from '@mui/material';
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import {
+  AppBar,
+  Toolbar,
+  CssBaseline,
+  Container,
+  Box,
+  IconButton,
+  createTheme,
+  ThemeProvider,
+  FormControlLabel,
+  Switch
+} from '@mui/material';
 import ImportContactsIcon from '@mui/icons-material/ImportContacts';
 import DirectionsCarIcon from '@mui/icons-material/DirectionsCar';
 import Diary from './components/Diary/Diary.tsx';
 import TripForm from './components/TripForm/TripForm.tsx';
-
+import { useState } from 'react';
 const App = () => {
+  const [darkMode, setDarkMode] = useState(() => {
+    const savedMode = localStorage.getItem('darkMode');
+    return savedMode ? JSON.parse(savedMode) : false;
+  });
+
+  const handleThemeChange = () => {
+    setDarkMode((prevMode: boolean) => {
+      const newMode = !prevMode;
+      localStorage.setItem('darkMode', JSON.stringify(newMode));
+      return newMode;
+    });
+  };
+
+  const theme = createTheme({
+    palette: {
+      mode: darkMode ? 'dark' : 'light',
+    },
+  });
+
   return (
     <Router>
-      <CssBaseline />
-      <AppBar position="static">
-        <Toolbar>
-          <Box display="flex" justifyContent="center" width="100%" sx={{ gap: 4 }}>
-            <IconButton color="inherit" component={Link} to="/create">
-              <DirectionsCarIcon />
-            </IconButton>
-            <IconButton color="inherit" component={Link} to="/diary">
-              <ImportContactsIcon />
-            </IconButton>
-          </Box>
-        </Toolbar>
-      </AppBar>
-      <Container>
-        <Routes>
-          <Route path="/" element={<TripForm />} />
-          <Route path="/create" element={<TripForm />} />
-          <Route path="/diary" element={<Diary />} />
-        </Routes>
-      </Container>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <AppBar position="static">
+          <Toolbar>
+            <Box display="flex" justifyContent="center" width="100%" sx={{ gap: 4 }}>
+              <IconButton color="inherit" component={Link} to="/create">
+                <DirectionsCarIcon />
+              </IconButton>
+              <IconButton color="inherit" component={Link} to="/diary">
+                <ImportContactsIcon />
+              </IconButton>
+              <FormControlLabel
+                control={<Switch checked={darkMode} onChange={handleThemeChange} color="default" />}
+                label="Dark Mode"
+              />
+            </Box>
+          </Toolbar>
+        </AppBar>
+        <Container>
+          <Routes>
+            <Route path="/" element={<TripForm />} />
+            <Route path="/create" element={<TripForm />} />
+            <Route path="/diary" element={<Diary />} />
+          </Routes>
+        </Container>
+      </ThemeProvider>
     </Router>
   );
 };
